de36840a8eb4a7e5cf7674125a5810c525b198a1
Ohmnyx <68547590+Ohmnyx@users.noreply.github.com>
2023-06-22 17:50:11 +0700

    Add files via upload

---
+++ resources/views/default.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Default</title>
+</head>
+
+<body>
+    <div class="container mt-4">
+        <h4>{{ $pageTitle }}</h4>
+        <hr>
+        <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3
+        border">
+            <div class="bi-house-fill me-3 fs-1"></div>
+            <h4 class="mb-0">Well done! this is {{ $pageTitle }}.</h4>
+        </div>
+    </div>
+
+</body>
+
+</html>
